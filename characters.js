3c5974a2d2ffa075303a389264229a6c4bacb95d
rush-chanOP <chandraarush01@gmail.com>
2021-08-27 17:50:30 +0530

    1st commit: sending characters and story files from staging area to local repo

---
+++ characters.js
@@ -0,0 +1,5 @@
+A short story is a piece of prose fiction that typically can be read in one sitting and focuses on a
+ self-contained incident or series of linked incidents, with the intent of evoking a single effect or 
+ mood.
+
+ ==============================================before first ADD=========================================
